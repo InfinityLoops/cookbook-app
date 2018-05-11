fb783b4588da512257dde7961311dc33ee8ba5d8
JackMarx <seriousfools@gmail.com>
2018-05-10 20:26:52 -0500

    update

--- runner.rb
+++ runner.rb
@@ -9,18 +9,31 @@ require 'unirest'
 # puts JSON.pretty_generate(response.body)
 
 # CREATE ACTION
-response = Unirest.post(
-                        "http://localhost:3000/api/recipes", 
-                        parameters: {
-                                      title: "baked squirrel",
-                                      chef: "Uncle billy joe bob",
-                                      ingredients: "roadkill",
-                                      directions: "walk out to road, pick up what ever is lying around, fry it",
-                                      prep_time: 90
-                                    }
-                        )
-puts JSON.pretty_generate(response.body)
+# response = Unirest.post(
+#                         "http://localhost:3000/api/recipes", 
+#                         parameters: {
+#                                       title: "baked squirrel",
+#                                       chef: "Uncle billy joe bob",
+#                                       ingredients: "roadkill",
+#                                       directions: "walk out to road, pick up what ever is lying around, fry it",
+#                                       prep_time: 90
+#                                     }
+#                         )
+# puts JSON.pretty_generate(response.body)
 
+# UPDATE ACTION
+recipe_id = 6
+runner_params = {
+                 title: "Mud Cake!!!"
+                }
+
+response = Unirest.patch(
+                         "http://localhost:3000/api/recipes/#{recipe_id}",
+                         parameters: runner_params
+                        )
+
+recipe_hash = response.body
+puts JSON.pretty_generate(recipe_hash)
 
 
 
